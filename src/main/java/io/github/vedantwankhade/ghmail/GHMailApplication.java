e63947b221b1b04c198a868e6a80d0ad0d186019
VedantWankhade <vedantwankhade17@gmail.com>
2022-07-03 18:49:51 +0530

    github oauth starter

---
+++ src/main/java/io/github/vedantwankhade/ghmail/GHMailApplication.java
@@ -0,0 +1,25 @@
+package io.github.vedantwankhade.ghmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GHMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GHMailApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public String user(@AuthenticationPrincipal OAuth2User principal) {
+		System.out.println(principal);
+		return principal.getAttribute("login");
+	}
+	
+
+}
