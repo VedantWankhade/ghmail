3be09040167e930bdfa9780d572d2f15ab708e17
VedantWankhade <vedantwankhade17@gmail.com>
2022-07-04 18:00:50 +0530

    Compose view and route

---
+++ src/main/java/io/github/vedantwankhade/ghmail/controller/ComposeController.java
@@ -0,0 +1,44 @@
+package io.github.vedantwankhade.ghmail.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import io.github.vedantwankhade.ghmail.model.Folder;
+import io.github.vedantwankhade.ghmail.repository.FolderRepository;
+import io.github.vedantwankhade.ghmail.service.FolderService;
+
+@Controller
+public class ComposeController {
+
+	@Autowired
+	private FolderRepository folderRepository;
+	
+	@Autowired
+	private FolderService folderService;
+	
+	@GetMapping(value = "/compose")
+	public String getComposePage(@AuthenticationPrincipal OAuth2User principal, Model model) {
+		if (principal == null || !StringUtils.hasText(principal.getAttribute("login")))
+			return "index";
+		
+		String userId = principal.getAttribute("login");
+		
+		model.addAttribute("userId", userId);
+		
+		List<Folder> userFolders = folderRepository.findAllByUserId(userId);
+		model.addAttribute("userFolders", userFolders);
+		
+		List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
+		model.addAttribute("defaultFolders", defaultFolders);
+		
+		return "compose-page";
+	}
+	
+}
