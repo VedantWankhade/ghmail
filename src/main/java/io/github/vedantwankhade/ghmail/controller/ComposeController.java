59fe06c878051b418435879afc39f772075ec8be
VedantWankhade <vedantwankhade17@gmail.com>
2022-07-04 19:12:43 +0530

    Send email and partial reply

--- src/main/java/io/github/vedantwankhade/ghmail/controller/ComposeController.java
+++ src/main/java/io/github/vedantwankhade/ghmail/controller/ComposeController.java
@@ -1,5 +1,6 @@
 package io.github.vedantwankhade.ghmail.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,12 +10,17 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import io.github.vedantwankhade.ghmail.model.Folder;
 import io.github.vedantwankhade.ghmail.repository.FolderRepository;
+import io.github.vedantwankhade.ghmail.service.EmailService;
 import io.github.vedantwankhade.ghmail.service.FolderService;
 
 @Controller
@@ -26,6 +32,9 @@ public class ComposeController {
 	@Autowired
 	private FolderService folderService;
 
+	@Autowired
+	private EmailService emailService;
+	
 	@GetMapping(value = "/compose")
 	public String getComposePage(@RequestParam(required = false) String to,
 			@AuthenticationPrincipal OAuth2User principal, Model model) {
@@ -42,14 +51,35 @@ public class ComposeController {
 		List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
 		model.addAttribute("defaultFolders", defaultFolders);
 
-		if (StringUtils.hasText(to)) {
-			List<String> uniqueToIds = Arrays.asList(to.split(",")).stream()
-					.map(id -> StringUtils.trimWhitespace(id)).filter(id -> StringUtils.hasText(id)).distinct()
-					.collect(Collectors.toList());
-
-			model.addAttribute("toIds", String.join(", ", uniqueToIds));
-		}
+		List<String> uniqueToIds = splitToIds(to);
+		model.addAttribute("toIds", String.join(", ", uniqueToIds));
+		
 		return "compose-page";
+	}
+
+	private List<String> splitToIds(String to) {
+		if (!StringUtils.hasText(to))
+			return new ArrayList<String>();
+
+		return Arrays.asList(to.split(",")).stream().map(id -> StringUtils.trimWhitespace(id))
+				.filter(id -> StringUtils.hasText(id)).distinct().collect(Collectors.toList());
+	}
+
+	@PostMapping(value = "/sendEmail")
+	public ModelAndView sendEmail(@RequestBody MultiValueMap<String, String> formDataMap,
+			@AuthenticationPrincipal OAuth2User principal) {
+
+		if (principal == null || !StringUtils.hasText(principal.getAttribute("login")))
+			return new ModelAndView("redirect:/");
+		
+		String from = principal.getAttribute("login");
+		List<String> toIds = splitToIds(formDataMap.getFirst("toIds"));
+		String subject = formDataMap.getFirst("subject");
+		String body = formDataMap.getFirst("body");
+
+		emailService.sendEmail(from, toIds, subject, body);
+		
+		return new ModelAndView("redirect:/?folder=Sent%20Items");
 	}
 
 }
---
+++ src/main/java/io/github/vedantwankhade/ghmail/service/EmailService.java
@@ -0,0 +1,60 @@
+package io.github.vedantwankhade.ghmail.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.datastax.oss.driver.api.core.uuid.Uuids;
+
+import io.github.vedantwankhade.ghmail.model.Email;
+import io.github.vedantwankhade.ghmail.model.EmailListItem;
+import io.github.vedantwankhade.ghmail.model.utilmodel.EmailListItemCompoundKey;
+import io.github.vedantwankhade.ghmail.repository.EmailListItemRepository;
+import io.github.vedantwankhade.ghmail.repository.EmailRepository;
+
+@Service
+public class EmailService {
+
+	@Autowired
+	private EmailRepository emailRepository;
+	
+	@Autowired
+	private EmailListItemRepository emailListItemRepository;
+	
+	public void sendEmail(String from, List<String> to, String subject, String body) {
+		
+		Email email = new Email();
+		email.setTo(to);
+		email.setFrom(from);
+		email.setSubject(subject);
+		email.setBody(body);
+		email.setTimeUUID(Uuids.timeBased());
+		
+		emailRepository.save(email);
+		
+		to.forEach(toId -> {
+			EmailListItem item = createEmailListItem(to, subject, email, toId, "Inbox");
+			emailListItemRepository.save(item);
+		});
+		
+		EmailListItem sentItem = createEmailListItem(to, subject, email, from, "Sent Items");
+		emailListItemRepository.save(sentItem);
+	}
+
+	private EmailListItem createEmailListItem(List<String> to, String subject, Email email, String itemOwner, String folder) {
+		EmailListItemCompoundKey key = new EmailListItemCompoundKey();
+		
+		key.setUserId(itemOwner);
+		key.setLabel(folder);
+		key.setTimeUUID(email.getTimeUUID());
+		
+		EmailListItem item = new EmailListItem();
+		
+		item.setKey(key);
+		item.setTo(to);
+		item.setSubject(subject);
+		item.setUnread(true);
+		return item;
+	}
+}
