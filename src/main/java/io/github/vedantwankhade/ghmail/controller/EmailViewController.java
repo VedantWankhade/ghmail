591d203591cbe20007cbbf5d652711e4bc4dfb96
VedantWankhade <vedantwankhade17@gmail.com>
2022-07-06 15:26:19 +0530

    unread decrement  + unread view

--- src/main/java/io/github/vedantwankhade/ghmail/controller/EmailViewController.java
+++ src/main/java/io/github/vedantwankhade/ghmail/controller/EmailViewController.java
@@ -12,12 +12,16 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import io.github.vedantwankhade.ghmail.model.Email;
+import io.github.vedantwankhade.ghmail.model.EmailListItem;
 import io.github.vedantwankhade.ghmail.model.Folder;
+import io.github.vedantwankhade.ghmail.model.utilmodel.EmailListItemCompoundKey;
 import io.github.vedantwankhade.ghmail.repository.EmailListItemRepository;
 import io.github.vedantwankhade.ghmail.repository.EmailRepository;
 import io.github.vedantwankhade.ghmail.repository.FolderRepository;
+import io.github.vedantwankhade.ghmail.repository.UnreadEmailStatsRepository;
 import io.github.vedantwankhade.ghmail.service.FolderService;
 
 @Controller
@@ -30,13 +34,16 @@ public class EmailViewController {
 	private EmailRepository emailRepository;
 	
 	@Autowired
+	private FolderService folderService;
+	
+	@Autowired
 	private EmailListItemRepository emailListItemRepository;
 	
 	@Autowired
-	private FolderService folderService;
+	private UnreadEmailStatsRepository unreadEmailStatsRepository;
 	
 	@GetMapping(value = "/emails/{id}")
-	public String emailView(@PathVariable UUID id, @AuthenticationPrincipal OAuth2User principal, Model model) {
+	public String emailView(@RequestParam String folder, @PathVariable UUID id, @AuthenticationPrincipal OAuth2User principal, Model model) {
 
 		if (principal == null || !StringUtils.hasText(principal.getAttribute("login")))
 			return "index";
@@ -50,8 +57,6 @@ public class EmailViewController {
 
 		List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
 		model.addAttribute("defaultFolders", defaultFolders);
-		
-		model.addAttribute("unreadStats", folderService.mapCountToLabels(userId));
 
 		Optional<Email> optionalEmail = emailRepository.findById(id);
 		
@@ -63,6 +68,25 @@ public class EmailViewController {
 		String toIds = String.join(", ", email.getTo());
 		
 		model.addAttribute("toIds", toIds);
+		
+		EmailListItemCompoundKey key = new EmailListItemCompoundKey();
+		key.setUserId(userId);
+		key.setLabel(folder);
+		key.setTimeUUID(email.getTimeUUID());
+		
+		Optional<EmailListItem> optionalEmailListItem = emailListItemRepository.findById(key);
+		
+		if (optionalEmailListItem.isPresent()) {
+			EmailListItem emailListItem = optionalEmailListItem.get();
+			
+			if (emailListItem.isUnread()) {
+				emailListItem.setUnread(false);
+				emailListItemRepository.save(emailListItem);
+				
+				unreadEmailStatsRepository.decrementUnreadCount(userId, folder);
+			}
+		}
+		model.addAttribute("unreadStats", folderService.mapCountToLabels(userId));
 		return "email-page";
 	}
 }
